f9acfe7f13907a13b8414cd9e5c97fe63639fe6f
Md Antor <md.antor1155@gmail.com>
2022-04-19 02:41:21 +0800

    name and title given to site with favicon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/HomePage/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+
+// document.body.style = 'background: #181717;';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
 
-
+     <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <hr />
+            <h4>This is footer part</h4>
+            <p>This website belog to <span>Md Antor</span></p>
+        </div>
+    );
+};
+
+export default Footer;
